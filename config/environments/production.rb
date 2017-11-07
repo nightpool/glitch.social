e2a5a4abd54b551b6c51c53609c04c1e9197b7d2
beatrix-bitrot <beatrix.bitrot@gmail.com>
2017-11-07 18:40:41 +0000

    memorial for tipsy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,8 @@ Rails.application.configure do
     'X-XSS-Protection'        => '1; mode=block',
     'Content-Security-Policy' => "frame-ancestors 'none'; object-src 'none'; script-src 'self' https://static.glitch.social; base-uri 'none';" , 
     'Referrer-Policy'         => 'no-referrer, strict-origin-when-cross-origin',
-    'Strict-Transport-Security' => 'max-age=63072000; includeSubDomains; preload'
+    'Strict-Transport-Security' => 'max-age=63072000; includeSubDomains; preload',
+    'X-Clacks-Overhead' => 'GNU Natalie Nguyen'
   }
 
   config.x.otp_secret = ENV.fetch('OTP_SECRET')
