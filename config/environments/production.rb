f8de6cebb3ffd90a86d2b7fe58f8c19abacad6c8
beatrix-bitrot <beatrix.bitrot@gmail.com>
2018-10-12 00:48:00 +0000

    tidy up headers in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -125,7 +125,7 @@ Rails.application.configure do
     'Permissions-Policy'     => 'interest-cohort=()',
     'Content-Security-Policy' => "frame-ancestors 'none'; object-src 'none'; script-src 'self' https://static.glitch.social; base-uri 'none';" , 
     'Referrer-Policy'         => 'same-origin',
-    'Strict-Transport-Security' => 'max-age=63072000; includeSubDomains; preload',
+    'Strict-Transport-Security' => 'max-age=63072000; includeSubDomains',
     'X-Clacks-Overhead' => 'GNU Natalie Nguyen'
   }
 
